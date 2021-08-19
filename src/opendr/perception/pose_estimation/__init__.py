97f4e6d27d368ddf74aa8f617eaa2da6b7761b98
Nikolaos Passalis <passalis@csd.auth.gr>
2021-08-19 14:43:11 +0300

    Demo update

--- src/opendr/perception/pose_estimation/__init__.py
+++ src/opendr/perception/pose_estimation/__init__.py
@@ -1,4 +1,4 @@
 from opendr.perception.pose_estimation.lightweight_open_pose.lightweight_open_pose_learner import \
     LightweightOpenPoseLearner
 
-from opendr.perception.pose_estimation.lightweight_open_pose.utilities import *
+from opendr.perception.pose_estimation.lightweight_open_pose.utilities import draw as draw_pose
